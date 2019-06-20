a73d4e171d4ef669b3b3b1ad2de967301d0bec60
Max Keller <max.keller427@gmail.com>
2019-06-19 20:51:29 -0600

    working on last step in fail

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -2,19 +2,78 @@ module.exports = {
   succeed,
   fail,
   repair,
-  get,
+  get
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement <= 19) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability,
+      enhancement: item.enhancement + 1
+    };
+    return newItem;
+  } else {
+    return item;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    if (item.durability < 5) {
+      const newItem = {
+        name: item.name,
+        durability: 0,
+        enhancement: item.enhancement
+      };
+      return newItem;
+    }
+    const newItem = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item.enhancement
+    };
+    return newItem;
+  } else if (item.enhancement === 15 || item.enhancement === 16) {
+    if (item.durability < 10) {
+      const newItem = {
+        name: item.name,
+        durability: 0,
+        enhancement: item.enhancement
+      };
+      return newItem;
+    }
+    const newItem = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement
+    };
+    return newItem;
+  } else {
+    if (item.durability === 0) {
+      const newItem = {
+        name: item.name,
+        durability: 0,
+        enhancement: item.enhancement
+      };
+      return newItem;
+    }
+    const newItem = {
+      name: item.name,
+      durability: item.durability - 1,
+      enhancement: item.enhancement
+    };
+    return newItem;
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  const newItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement
+  };
+  return newItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,101 @@
-const enhancer = require('./enhancer.js');
+const { repair, succeed, fail, get } = require("./enhancer.js");
 // test away!
+
+describe("the repair function", () => {
+  it("should increase duribility to 100", () => {
+    const sword = { durability: 23, name: "sword", enhancement: 14 };
+
+    const expected = { durability: 100, name: "sword", enhancement: 14 };
+
+    expect(repair(sword)).toEqual(expected);
+  });
+});
+
+describe("the succeed function", () => {
+  it("should increase the enhancement by 1", () => {
+    const bow = {
+      name: "bow",
+      enhancement: 10,
+      durability: 40
+    };
+
+    const arrow = {
+      name: "arrow",
+      enhancement: 20,
+      durability: 40
+    };
+
+    const expected1 = {
+      name: "bow",
+      enhancement: 11,
+      durability: 40
+    };
+
+    const expected2 = {
+      name: "arrow",
+      enhancement: 20,
+      durability: 40
+    };
+
+    expect(succeed(bow)).toEqual(expected1);
+    expect(succeed(arrow)).toEqual(expected2);
+  });
+});
+
+describe("the fail function", () => {
+  it("should reduce dur. by 5 if enh < 15, reduce the dur. by 10 if enh is 15 or 16, reduce dur by 1 if enh is 17 or more", () => {
+    const staff = {
+      name: "staff",
+      durability: 2,
+      enhancement: 5
+    };
+
+    const scimitar = {
+      name: "scimitar",
+      durability: 6,
+      enhancement: 14
+    };
+
+    const shortSword = {
+      name: "short sword",
+      durability: 9,
+      enhancement: 15
+    };
+
+    const mace = {
+      name: "mace",
+      durability: 19,
+      enhancement: 16
+    };
+
+    //failed
+    const failedStaff = {
+      name: "staff",
+      durability: 0,
+      enhancement: 5
+    };
+
+    const failedScimitar = {
+      name: "scimitar",
+      durability: 1,
+      enhancement: 14
+    };
+
+    const failedShortSword = {
+      name: "short sword",
+      durability: 0,
+      enhancement: 15
+    };
+
+    const failedMace = {
+      name: "mace",
+      durability: 9,
+      enhancement: 16
+    };
+
+    expect(fail(staff)).toEqual(failedStaff);
+    expect(fail(scimitar)).toEqual(failedScimitar);
+    expect(fail(shortSword)).toEqual(failedShortSword);
+    expect(fail(mace)).toEqual(failedMace);
+  });
+});
